f46b9a7f539b5cb97149e075d13978e2f493f679
RashiGajinkar <88249910+RashiGajinkar@users.noreply.github.com>
2021-08-07 23:35:54 +0530

    JavaScript Assignment

---
+++ JavaScript/Second1.js
@@ -0,0 +1,4 @@
+const names = ["Momo", "Sana", "Jihyo", "Mina","Tzuyu"];
+        
+        console.log(`Here the names names are ${names}`);
+        console.log(`Now the reverse order of names are ${names.reverse()}`)
---
+++ JavaScript/second2.js
@@ -0,0 +1,20 @@
+function NumGen(n)
+{
+ 
+    
+    for(let j = 1; j < n + 1; j++)
+    {
+ 
+        
+        if (j % 5 == 0 )
+            document.write(j + " ");
+    }
+    return n;
+}
+ 
+
+    let N = 50;
+     
+    
+    NumGen(N);
+     
